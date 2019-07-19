60f3473d00b8c95d2ff8190ffab44f1adde3aa30
samuelkayode <oluwakayodesamuel007@gmail.com>
2019-07-19 15:25:10 +0100

    try

--- src/App.js
+++ src/App.js
@@ -16,65 +16,72 @@ class App extends React.Component {
     error: false
   }
 
-getWeather = async (e) => {
+  getWeather = async (e) => {
 
-  
-const city = e.target.elements.city.value;
 
-const country = e.target.elements.country.value;
+    const city = e.target.elements.city.value;
 
-e.preventDefault();
+    const country = e.target.elements.country.value;
 
-  const api_call = await 
-  Axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
-  
-  const response = api_call.data;
-  if(city && country){
-  this.setState({
-    temperature: response.main.temp,
-    city: response.name,
-    country: response.sys.country,
-    humidity: response.main.humidity,
-    description: response.weather[0].description,
-    error: ""
-  })
-}else{
-  this.setState({
-       error: "Please enter the values..."
-})
-  
-  console.log(response);
-}
-  
-}
-render(){
-  return (
-    <div>
-    <div className="wrapper">
-     <div className="main">
-      <div className="container">
-       <div className="row">
-         <div className="col-xs-5 title-container">
-           <Titles />
-         </div>
-         <div className="col-xs-7 form-container">
-           <Form loadWeather={this.getWeather} />
-           <Weather
-             temperature={this.state.temperature}
-             city={this.state.city}
-             country={this.state.country}
-             humidity={this.state.humidity}
-             description={this.state.description}
-             error={this.state.error}
-           />
-         </div>
+    e.preventDefault();
+    try {
+      const api_call = await
+        Axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+
+      const response = api_call.data;
+      console.log(response);
+      if (city && country) {
+        this.setState({
+          temperature: response.main.temp,
+          city: response.name,
+          country: response.sys.country,
+          humidity: response.main.humidity,
+          description: response.weather[0].description,
+          error: ""
+        })
+      } else {
+        this.setState({
+          error: "Please enter the values..."
+        })
+      }
+    } catch (error) {
+      console.log(error);
+      this.setState({
+        error: 'Can not fetch weather info at this time.'
+      });
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form loadWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-       </div>
       </div>
-     </div>
-   </div>
-  )
+    )
+  }
 }
-}
+
 
 export default App;
